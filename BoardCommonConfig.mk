8396beef47952c13235b4125bcbfc34426c3f12a
Dominggoes Isakh <drjisakh@gmail.com>
2018-01-21 11:48:41 +0100

    smdk4412-common: Enable debug info
    Change-Id: I40cdd1ef8b375f564247fba2f8e345173a318769

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -66,6 +66,9 @@ endif
 
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
+# Generate debug info
+PRODUCT_DEX_PREOPT_BOOT_FLAGS += --generate-mini-debug-info
+
 # Filesystem
 BOARD_NAND_PAGE_SIZE := 4096
 BOARD_NAND_SPARE_SIZE := 128
