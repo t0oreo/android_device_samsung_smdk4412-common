43e8fced19e13ce9b6e666b18854272898619d59
Dominggoes Isakh <drjisakh@gmail.com>
2018-01-21 11:33:32 +0100

    smdk4412-common: Enable legacy f_adb interface
    Change-Id: I334e541ee234f7b34860d345720cd57b9929eb7a

--- common.mk
+++ common.mk
@@ -41,6 +41,7 @@ PRODUCT_COPY_FILES += \
 # HAL
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
+    vendor.lineage.power@1.0-service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.keymaster@3.0-impl \
